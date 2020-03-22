be94bf12673ba279af8733d47d33a86b4110c41a
Priscilla Alcalde Melo <10372380+priscillamelo@users.noreply.github.com>
2020-03-22 12:00:18 +0000

    Create character_input.py
    Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
    
    Extras:
    
    Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
    Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
    
    https://www.practicepython.org/exercise/2014/01/29/01-character-input.html

---
+++ character_input.py
@@ -0,0 +1,13 @@
+name = input("What's your name?")
+
+print("Nice to meet you " + name +" !")
+
+age = input("What's your age?")
+
+age = int(age)
+
+print("So you're already " + str(age) + " years old, " + name + " !")
+
+year_hundred = (100 - age) + 2020
+
+print("You'll be 100 years old in the year " + str(year_hundred) + "...")
