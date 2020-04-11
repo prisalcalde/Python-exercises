cfddac8c924fcdd0d1ed65e3fc87f0cc2572244a
Priscilla Alcalde Melo <10372380+priscillamelo@users.noreply.github.com>
2020-04-11 12:03:54 +0100

    Create string_lists.py
    Ask the user for a string and print out whether this string is a palindrome or not. A palindrome is a string that reads the same forwards and backwards.

---
+++ string_lists.py
@@ -0,0 +1,39 @@
+
+# Ask the user for a string and print out whether this string is a palindrome or not.
+# A palindrome is a string that reads the same forwards and backwards.
+
+# # using string reverse
+name = input("What's your name? ")
+
+reverse_name = name[::-1] # reverses the string provided by the user
+
+print(name)
+print(reverse_name)
+
+if name == reverse_name:
+    print("Your name is palindrome!")
+else:
+    print("Your name is not a palindrome!")
+
+# # using string reverse
+name = input("What's your name? ")
+
+reverse_name = name[::-1] # reverses the string provided by the user
+
+print(name)
+print(reverse_name)
+
+if name == reverse_name:
+    print("Your name is palindrome!")
+else:
+    print("Your name is not a palindrome!")
+
+
+
+
+
+
+
+
+
+
