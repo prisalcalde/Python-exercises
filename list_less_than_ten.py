8cd5cb99d1ef148521b40dd241bd592bdf654bf8
Priscilla Alcalde Melo <10372380+priscillamelo@users.noreply.github.com>
2020-04-11 17:03:50 +0100

    Update list_less_than_ten.py

--- list_less_than_ten.py
+++ list_less_than_ten.py
@@ -1,38 +1,54 @@
+# Take a list, say for example this one:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5
 
-"""
-Write a program that prints out all the elements of the list that are less than 5
-
-1. Instead of printing the elements one by one, make a new list that has all the
-elements less than 5 from this list in it and print out this new list.
-
-2. Write this in one line of Python.
-
-3. Ask the user for a number and return a list that contains only elements from the
-original list a that are smaller than that number given by the user.
-"""
-
-# 1
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-for item in a:
-    if item < 5:
-        print(item)
+for i in a:
+    if i < 5:
+        print(i)
 
-# 2
+# ----------------------------------------
+# Instead of printing the elements one by one, make a new list that has all the
+# elements less than 5 from this list in it and print out this new list.
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
-[print(item) for item in a if item < 5]
-
-
-# 3
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
-num = int(input("Give me a number: "))
-
 b = []
 
-for item in a:
-    if num < item:
-        b.append(item)
+for i in a:
+    if i < 5:
+        b.append(i)
 
 print(b)
+
+# ----------------------------------------
+# Write this in one line of Python
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#
+# b = [i for i in a if i < 5] # using list comprehension
+# print(b)
+
+# ----------------------------------------
+# Ask the user for a number and return a list that contains only elements
+# from the original list a that are smaller than that number given by the user.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+num = int(input("Give me a number:\n"))
+
+c = []
+
+for i in a:
+    if i < num:
+        c.append(i)
+
+print(c)
+
+# or
+
+num = int(input("Give me a number:\n"))
+
+c = [i for i in a if i < num]
+
+print(c)
