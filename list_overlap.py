b77c4d80c8e0f7c6a9a864b1c3de2c2c9f086939
Priscilla Alcalde Melo <10372380+priscillamelo@users.noreply.github.com>
2020-04-13 21:39:42 +0100

    Update list_overlap.py

--- list_overlap.py
+++ list_overlap.py
@@ -1,22 +1,34 @@
 # Write a program that returns a list that contains only the elements that are common 
 # between the lists (without duplicates). Make sure your program works on two lists of different sizes
 
+# using set
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-# checking matching numbers and adding to list c
 c = []
-for item in a:
-    if item == item in b:
-        c.append(item)
 
-# adding only the non duplicate items to list d
-d = []
-for item in c:
-    if item not in d:
-        d.append(item)
+for i in a:
+    if i == i in b:
+        c.append(i)
+        d = list(set(c))
 
-print("New list with matching elements:", str(d))
+print(d)
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# # checking matching numbers and adding to list c
+# c = []
+# for item in a:
+#     if item == item in b:
+#         c.append(item)
+
+# # adding only the non duplicate items to list d
+# d = []
+# for item in c:
+#     if item not in d:
+#         d.append(item)
+
+# print("New list with matching elements:", str(d))
 
 
 # generating two random lists to test the code
