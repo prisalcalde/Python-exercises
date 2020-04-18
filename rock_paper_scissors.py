9133205442fa54ce9f55c0e2594b4d37d735ff6b
Priscilla Alcalde Melo <10372380+prisalcalde@users.noreply.github.com>
2020-04-18 16:06:42 +0100

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,20 +1,9 @@
-
 # Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them,
 # print out a message of congratulations to the winner, and ask if the players want to start a new game)
 # Remember the rules:
 # - Rock beats scissors
 # - Scissors beats paper
 # - Paper beats rock
-
-# 1. ask the two players input
-# 2. when the inputs are different, check if they are "rock", "paper" or "scissors"
-# 3. if the inputs are rock and paper, paper wins
-# 4. inf the inputs are rock and scissors, rock wins
-# 5. if the inputs are scissors and paper, scissors win
-# 6. print out a message saying congratulations to the winner
-# 7. ask if they want to play a new game
-# 8. if the inputs are the same, start again
-# 9. end
 
 def rock_paper_scissors():
     player_1 = input("What's your name? ")
@@ -76,7 +65,3 @@ def rock_paper_scissors():
             player_2_answer = input(player_2 + ": Rock, paper or scissors? ")
 
 rock_paper_scissors()
-
-
-
-
