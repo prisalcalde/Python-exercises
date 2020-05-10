008303a054934fc9ab157e86a8335ecee7edddef
Priscilla Alcalde Melo <10372380+prisalcalde@users.noreply.github.com>
2020-05-10 12:45:43 +0100

    Update Python_basic_exercises.py

--- Python_basic_exercises.py
+++ Python_basic_exercises.py
@@ -22,3 +22,18 @@ print(sys.version_info)
 from datetime import datetime
 now = datetime.now()
 print(now)
+
+# 4. Write a Python program which accepts the radius of a circle from the
+# user and compute the area. Go to the editor
+# Sample Output:
+# r = 1.1
+# Area = 3.8013271108436504
+
+import math
+
+radius = float(input("Radius of a circle: "))
+
+pi = math.pi
+area = pi*radius*radius
+
+print("The area of a circle with radius " + str(radius) + " is " + str(area))
