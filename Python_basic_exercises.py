e72fe11006098748ee212747c8c20d3050c87f2e
Priscilla Alcalde Melo <10372380+prisalcalde@users.noreply.github.com>
2020-05-17 10:49:37 +0100

    Update Python_basic_exercises.py

--- Python_basic_exercises.py
+++ Python_basic_exercises.py
@@ -161,3 +161,16 @@ may2020 = calendar.month(2020, 0o5)
 
 print(may2020)
 
+#13. Write a Python program to print the following here document.
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+
+print("""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+""")
