541122a72de062b6025d3ab772bad4db0aaa40bb
Priscilla Alcalde Melo <10372380+prisalcalde@users.noreply.github.com>
2020-05-30 19:09:05 +0100

    Update Decode_a_web_page.py

--- Decode_a_web_page.py
+++ Decode_a_web_page.py
@@ -5,6 +5,22 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.theguardian.com/uk"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
-print(soup)
+content = requests.get(url)
+soup = BeautifulSoup(content.text, "html.parser")
+# print(soup)
+
+# printing the website title
+print(soup.title)
+
+# The article titles are all on <span class="js-headline-text"></span>
+article_titles = soup.find_all('span', class_="js-headline-text")
+
+# Prints a list of article titles
+print(article_titles)
+
+# Prints each article title in a line
+for article_title in article_titles:
+    # print(article_title)  # prints the titles with the tags
+    
+    # extracts and prints only the titles without the tags
+    print(article_title.text)
