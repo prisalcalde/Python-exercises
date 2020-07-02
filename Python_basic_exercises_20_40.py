0107ce482437552c1071497e432fe6055ca94134
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-07-02 21:30:41 +0100

    Update Python_basic_exercises_20_40.py

--- Python_basic_exercises_20_40.py
+++ Python_basic_exercises_20_40.py
@@ -54,3 +54,19 @@ def vowel_check(passed_letter):
 
 vowel_check("e")
 vowel_check("k")
+
+# 25. Write a Python program to check whether a specified value is contained in a group of values.
+# Test Data:
+# 3 -> [1, 5, 8, 3] : True
+# -1 -> [1, 5, 8, 3] : False
+
+def check_value(value, group_values):
+
+    for i in group_values:
+        if i == value:
+            return True
+    return False
+
+
+print(check_value(3, [1, 5, 8, 3]))
+print(check_value(-1, [1, 5, 8, 3]))
