b55064395d1f4aaecf90939c3f3ac6bf9686518d
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-08-15 12:04:07 +0100

    Update Write_to_a_file.py

--- Write_to_a_file.py
+++ Write_to_a_file.py
@@ -5,23 +5,21 @@
 # Extras:
 # Ask the user to specify the name of the output file that will be saved.
 
-# Code from 'How to Decode a Website':
-
+# Code from 'How to Decode a Website' exercise:
 import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.theguardian.com/uk"
 content = requests.get(url)
 soup = BeautifulSoup(content.text, "html.parser")
-# print(soup)
 
 # The article titles are all on <a href="" class="js-headline-text"></a>
 article_titles = soup.find_all('a', class_="js-headline-text")
 
-# # 1. Writing today's headlines on todays_news.txt file
-# with open('todays_news.txt', 'w') as file:
-#     for article_title in article_titles:
-#         file.write(str(article_title.text))
+# 1. Writing today's headlines on todays_news.txt file
+with open('todays_news.txt', 'w') as file:
+    for article_title in article_titles:
+        file.write(str(article_title.text))
 
 # 2. Asking the user to specify the name of the output file that will be saved
 user_file = input("What's the name of the file that you want to save today's news? \n")
