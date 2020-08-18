68cf5443d824f250d8ee3c1550b13f528463f336
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-08-18 19:04:43 +0100

    Update string_lists.py

--- string_lists.py
+++ string_lists.py
@@ -15,20 +15,6 @@ if name == reverse_name:
 else:
     print("Your name is not a palindrome!")
 
-# # using string reverse
-name = input("What's your name? ")
-
-reverse_name = name[::-1] # reverses the string provided by the user
-
-print(name)
-print(reverse_name)
-
-if name == reverse_name:
-    print("Your name is palindrome!")
-else:
-    print("Your name is not a palindrome!")
-
-
 
 
 
