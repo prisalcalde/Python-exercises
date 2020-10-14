21723a91d03ebfcde756350211e89164e7e39e01
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-10-14 08:43:05 +0100

    Update Reverse_word_order.py

--- Reverse_word_order.py
+++ Reverse_word_order.py
@@ -1,12 +1,5 @@
 # Write a program (using functions!) that asks the user for a long string containing multiple words.
 # Print back to the user the same string, except with the words in backwards order.
-# For example, say I type the string:
-#
-#   My name is Michele
-# Then I would see the string:
-#
-#   Michele is name My
-# shown back to me.
 
 def reverse_word_order():
     phrase = input("Give me a long phrase: ")
@@ -16,3 +9,32 @@ def reverse_word_order():
 
 
 print(reverse_word_order())
+
+
+#Reverse word:
+
+# def every_other_letter(word):
+#   new_word = ''
+#   i = 0
+#   for i in range(len(word)):
+#     if i % 2 == 0:
+#       print(i)
+#       new_word += word[i]
+#   return new_word
+
+
+# print(every_other_letter('Telephone'))
+
+
+# alternative solution using range jumping at every second index
+def every_other_letter(word):
+  new_word = ''
+  i = 0
+  for i in range(0, len(word), 2):
+    # if i % 2 == 0:
+    new_word += word[i]
+  return new_word
+
+print(every_other_letter('Telephone'))
+
+
