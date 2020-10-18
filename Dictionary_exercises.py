2d9298df58cb87ffcae5130fcc25a9b5c8c19312
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-10-18 10:54:47 +0100

    Update Dictionary_exercises.py

--- Dictionary_exercises.py
+++ Dictionary_exercises.py
@@ -70,4 +70,25 @@ def unique_values(my_dictionary):
     if value not in unique_values:
       unique_values.append(value)
   return len(unique_values)
+
+#Create a function named count_first_letter that takes a dictionary named names as a parameter. names should be a dictionary where 
+# the key is a last name and the value is a list of first names. For example, the dictionary might look like this:
+# names = {"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}
+# The function should return a new dictionary where each key is the first letter of a last name, and the value is the number of people 
+# whose last name begins with that letter.
+# So in example above, the function would return:
+# {"S" : 4, "L": 3}
+
+
+def count_first_letter(names):
+  # key - first letter of the last name
+  # value - number of people whose last name begines with that letter
+  new_dictionary = {}
+  for key, value in names.items():
+    first_letter = key[0]
+    if first_letter not in new_dictionary:
+      new_dictionary[first_letter] = 0
+    new_dictionary[first_letter] += len(value)  
+  return new_dictionary
+
     
