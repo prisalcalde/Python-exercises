90a232c675a75657eb0417ac483b3d61186def93
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-10-26 07:37:23 +0000

    Create Classes_restaurant_menu.py

---
+++ Classes_restaurant_menu.py
@@ -0,0 +1,46 @@
+# Making the Menus --------------------------------------------
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+
+  def __repr__(self):
+    return '{} available from {} to {}'.format(self.name, self.start_time, self.end_time) 
+
+  def calculate_bill(self, purchased_items):
+    bill = 0
+    for purchased_item in purchased_items:
+      if purchased_item in self.items:
+        bill += self.items[purchased_item]
+    return bill
+
+# Brunch menu
+brunch_menu = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+brunch = Menu('Brunch', brunch_menu, 1100, 1600)
+
+# Early bird menu
+early_bird_menu = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+early_bird = Menu('Early bird', early_bird_menu, 1500, 1800)
+
+print(brunch)
+
+# Dinner bird menu
+dinner_menu = {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}
+dinner = Menu('Dinner', dinner_menu, 1700, 2300)
+
+# Kids bird menu
+kids_menu = {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}
+kids = Menu('Kids',kids_menu , 1100, 2100)
+
+print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+print(early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
