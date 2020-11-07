b7619e3b2d69ad8edcd7a4f5d3ae60ced1006385
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-11-07 10:18:17 +0000

    Update String_formatting.py

--- String_formatting.py
+++ String_formatting.py
@@ -13,3 +13,20 @@ print(introduction("John", "Lennon"))
 # should print Angelou, John Lennon
 print(introduction("Priscilla", "Melo"))
 # should print Priscilla, Prisclla Melo
+
+# -----------------------------------------------------------
+
+# Some say that every one year of a human’s life is equivalent to seven years of a dog’s life. 
+# Write a function named dog_years() that has two parameters named name and age.
+# The function should compute the age in dog years and return the following string:
+# "{name}, you are {age} years old in dog years"
+# Test this function with your name and your age!
+
+def dog_years(name, age):
+  age = 7 * age
+  return f"{name}, you are {age} years old in dog years"
+
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
