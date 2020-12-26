d831f67d1b424803896774f0762004667041b2cb
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2020-12-26 11:03:56 +0000

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -69,3 +69,11 @@ class HashMap:
       retrieval_collisions += 1
 
     return
+
+hash_map = HashMap(15)
+hash_map.assign('gabbro','igneous')
+hash_map.assign('sandstone','sedimentary')
+hash_map.assign('gneiss','metamorphic')
+print(hash_map.retrieve('gabbro'))
+print(hash_map.retrieve('sandstone'))
+print(hash_map.retrieve('gneiss'))
