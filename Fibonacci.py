cbda142c3a328be3826b9696800ab7f8a895a4f6
Priscilla Alcalde Melo <melo.priscilla@gmail.com>
2021-02-27 19:48:41 +0000

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,28 +1,13 @@
-# Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. Take this opportunity
-# to think about how you can use functions. Make sure to ask the user to enter the number of numbers in the sequence to
-# generate. (Hint: The Fibonnaci sequence is a sequence of numbers where the next number in the sequence is the sum of the 
-# previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+def fibonacci(n):
+  fibs = [0, 1]
 
-# function that returns a fibonacci sequence (list)
-def fibonacci(num = int(input("How many fibonacci numbers would you like to generate? "))):
-    
-    i = 1
+  if n <= len(fibs) -1:
+    return n
 
-    if num == 0:
-        fib = []
-    elif num == 1:
-        fib = [1]
-    elif num == 2:
-        fib = [1,1]
-    elif num > 2:
-        fib = [1,1]
-        # num - 1 is the index of the last item of the list
-        while i < (num - 1):
-            fib.append(fib[i] + fib[i-1])
-            i += 1
-    return fib
+  while n > len(fibs) -1:
+    fibs.append(fibs[-1] + fibs[-2])
 
-print(fibonacci())
+  return fibs[n]
 
 
 
